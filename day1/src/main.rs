b4925342c4b53fdf09f0c255d0df3d05fc011ff1
Andreas Andersson <andreas@neoboid.com>
2022-12-01 06:08:49 +0100

    1: Solve part 1

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -1,25 +1,22 @@
-#[macro_use]
-extern crate lazy_static;
-extern crate regex;
-
 use std::env;
 use std::fs;
 
 fn solve_part1(input: &str) -> usize {
-    lazy_static! {
-        static ref RE: regex::Regex = regex::Regex::new(r"(\d*) ([a-z]*)").unwrap();
-    }
-    let mut sum = 0;
+    let mut cals = 0;
+    let mut max_cals = 0;
     for line in input.lines() {
-        if let Some(captures) = RE.captures(line) {
-            if captures.len() == 3 {
-                let count = captures.get(1).unwrap().as_str().parse::<usize>().unwrap();
-                let _thing = captures.get(2).unwrap().as_str();
-                sum += count;
-            }
+        if line.len() > 0 {
+            let count = line.parse::<usize>().unwrap();
+            cals += count;
+        } else {
+            max_cals = if cals > max_cals { cals } else { max_cals };
+            cals = 0;
         }
     }
-    sum
+    if cals > 0 {
+        max_cals = if cals > max_cals { cals } else { max_cals };
+    }
+    max_cals
 }
 
 fn solve_part2(input: &str) -> usize {
@@ -48,12 +45,24 @@ mod tests_day1 {
     use super::*;
 
     const EXAMPLE1: &str = "
-3 seals
-4 quacks";
+1000
+2000
+3000
+
+4000
+
+5000
+6000
+
+7000
+8000
+9000
+
+10000";
 
     #[test]
     fn test1_1() {
-        assert_eq!(solve_part1(EXAMPLE1), 7);
+        assert_eq!(solve_part1(EXAMPLE1), 24000);
     }
 
     const EXAMPLE2: &str = "";
