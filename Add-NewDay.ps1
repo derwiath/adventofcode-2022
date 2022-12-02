3e545ceaff93874e6159447e1daf8e81edd62577
Andreas Andersson <andreas@neoboid.com>
2022-12-02 05:52:27 +0100

    Add scipt to copy dayx to new day

---
+++ Add-NewDay.ps1
@@ -0,0 +1,28 @@
+Param(
+	[parameter(Mandatory=$true)]
+	[ValidateRange(1, 25)]
+	[int]
+	$Day,
+
+	[String]
+	$SourcePackage="dayx"
+)
+$TargetPackage="day$Day"
+$TargetToml = "$TargetPackage/Cargo.toml"
+$TargetMain= "$TargetPackage/src/main.rs"
+$WorkspaceToml = "Cargo.toml"
+$TempFile = ".tempFile"
+
+Copy-Item -Recurse $SourcePackage -Destination $TargetPackage
+
+(Get-Content -Path $TargetToml ) -Replace $SourcePackage, $TargetPackage | Add-Content -Path $TempFile
+Remove-Item $TargetToml
+Move-Item -Force -Path $TempFile -Destination $TargetToml
+
+(Get-Content -Path $TargetMain ) -Replace $SourcePackage, $TargetPackage | Add-Content -Path $TempFile
+Move-Item -Force -Path $TempFile -Destination $TargetMain
+
+$Search="`"$SourcePackage`""
+$Replace ="`"$SourcePackage`", `"$TargetPackage`""
+(Get-Content -Path $WorkspaceToml) -Replace $Search, $Replace | Add-Content -Path $TempFile
+Move-Item -Force -Path $TempFile -Destination $WorkspaceToml
