9df71c6d44bf24b972f133e20afc3eaa16c55833
Andreas Andersson <andreas@neoboid.com>
2022-12-02 22:26:39 +0100

    2: Solve part2

--- day2/src/main.rs
+++ day2/src/main.rs
@@ -12,6 +12,13 @@ enum Pick {
     Scizzors,
 }
 
+#[derive(PartialEq, Debug, Copy, Clone)]
+enum Outcome {
+    Lose,
+    Draw,
+    Win,
+}
+
 impl Pick {
     fn new(s: &str) -> Pick {
         match s {
@@ -22,23 +29,66 @@ impl Pick {
         }
     }
 
-    fn play(&self, other: &Pick) -> usize {
+    fn play(&self, other: &Pick) -> Outcome {
         match *self {
             Pick::Rock => match *other {
-                Pick::Rock => 1 + 3,
-                Pick::Paper => 1,
-                Pick::Scizzors => 1 + 6,
+                Pick::Rock => Outcome::Draw,
+                Pick::Paper => Outcome::Lose,
+                Pick::Scizzors => Outcome::Win,
             },
             Pick::Paper => match *other {
-                Pick::Rock => 2 + 6,
-                Pick::Paper => 2 + 3,
-                Pick::Scizzors => 2 + 0,
+                Pick::Rock => Outcome::Win,
+                Pick::Paper => Outcome::Draw,
+                Pick::Scizzors => Outcome::Lose,
             },
             Pick::Scizzors => match *other {
-                Pick::Rock => 3 + 0,
-                Pick::Paper => 3 + 6,
-                Pick::Scizzors => 3 + 3,
+                Pick::Rock => Outcome::Lose,
+                Pick::Paper => Outcome::Win,
+                Pick::Scizzors => Outcome::Draw,
             },
+        }
+    }
+
+    fn score(&self) -> usize {
+        match *self {
+            Pick::Rock => 1,
+            Pick::Paper => 2,
+            Pick::Scizzors => 3,
+        }
+    }
+
+    fn pick_wins_over_self(&self) -> Pick {
+        match *self {
+            Pick::Rock => Pick::Paper,
+            Pick::Paper => Pick::Scizzors,
+            Pick::Scizzors => Pick::Rock,
+        }
+    }
+
+    fn pick_looses_to_self(&self) -> Pick {
+        match *self {
+            Pick::Rock => Pick::Scizzors,
+            Pick::Paper => Pick::Rock,
+            Pick::Scizzors => Pick::Paper,
+        }
+    }
+}
+
+impl Outcome {
+    fn new(s: &str) -> Outcome {
+        match s {
+            "X" => Outcome::Lose,
+            "Y" => Outcome::Draw,
+            "Z" => Outcome::Win,
+            _ => panic!("{} is not valid outcome", s),
+        }
+    }
+
+    fn score(&self) -> usize {
+        match *self {
+            Outcome::Win => 6,
+            Outcome::Draw => 3,
+            Outcome::Lose => 0,
         }
     }
 }
@@ -56,8 +106,16 @@ fn solve_part1(input: &str) -> usize {
                 let pick2_str = captures.get(2).unwrap().as_str();
                 let pick1 = Pick::new(pick1_str);
                 let pick2 = Pick::new(pick2_str);
-                let score = pick2.play(&pick1);
-                println!("{:?} {:?} -> {}", &pick1, &pick2, score);
+                let outcome = pick2.play(&pick1);
+                let score = pick2.score() + outcome.score();
+                println!(
+                    "{:?} {:?} -> {} ({} + {})",
+                    &pick1,
+                    &pick2,
+                    score,
+                    pick2.score(),
+                    outcome.score()
+                );
                 assert!(score > 0);
                 sum += score
             }
@@ -69,7 +127,42 @@ fn solve_part1(input: &str) -> usize {
 }
 
 fn solve_part2(input: &str) -> usize {
-    input.len()
+    lazy_static! {
+        static ref RE: regex::Regex = regex::Regex::new(r"([A-C]) ([X-Z])").unwrap();
+    }
+    let mut sum = 0;
+    for line in input.lines() {
+        if let Some(captures) = RE.captures(line) {
+            assert!(captures.len() == 3);
+            if captures.len() == 3 {
+                let pick1_str = captures.get(1).unwrap().as_str();
+                let pick2_str = captures.get(2).unwrap().as_str();
+                let pick1 = Pick::new(pick1_str);
+                let outcome = Outcome::new(pick2_str);
+                let pick2 = match outcome {
+                    Outcome::Win => pick1.pick_wins_over_self(),
+                    Outcome::Lose => pick1.pick_looses_to_self(),
+                    Outcome::Draw => pick1.clone(),
+                };
+
+                let score = pick2.score() + outcome.score();
+                println!(
+                    "{:?} {:?} -> {} ({} + {})",
+                    &pick1,
+                    &pick2,
+                    score,
+                    pick2.score(),
+                    outcome.score()
+                );
+                assert!(score > 0);
+                sum += score
+            }
+        } else if line.trim().len() > 0 {
+            print!("line: \"{}\"", line);
+            panic!("Failed to parse line");
+        }
+    }
+    sum
 }
 
 fn main() {
@@ -103,10 +196,8 @@ C Z";
         assert_eq!(solve_part1(EXAMPLE1), 15);
     }
 
-    const EXAMPLE2: &str = "15";
-
     #[test]
     fn test2_1() {
-        assert_eq!(solve_part2(EXAMPLE2), 0);
+        assert_eq!(solve_part2(EXAMPLE1), 12);
     }
 }
