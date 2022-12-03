cff2cda8513aa1d26b069572e31211e44980427a
Andreas Andersson <andreas@neoboid.com>
2022-12-03 12:54:45 +0100

    3: Remove debug prints

--- day3/src/main.rs
+++ day3/src/main.rs
@@ -13,15 +13,11 @@ fn solve_part1(input: &str) -> u32 {
         let compartment1 = &line[0..compartment_size];
         let compartment2 = &line[compartment_size..];
 
-        println!("1: {}", compartment1);
-        println!("2: {}", compartment2);
-
         let prio = compartment1
             .chars()
             .filter(|c| compartment2.contains(|c2| &c2 == c))
             .take(1)
             .map(|c| {
-                println!(" both: {}", c);
                 let value = c as u32;
                 if value >= LOWER_A_VALUE && value <= LOWER_Z_VALUE {
                     value - LOWER_A_VALUE + 1
@@ -33,7 +29,6 @@ fn solve_part1(input: &str) -> u32 {
             })
             .fold(0, |acc, prio| acc + prio);
 
-        println!("{}", prio);
         sum += prio
     }
     sum
@@ -45,17 +40,12 @@ fn solve_part2(input: &str) -> u32 {
     while let Some(line0) = lines_iter.next() {
         let line1 = lines_iter.next().unwrap();
         let line2 = lines_iter.next().unwrap();
-        println!("1: {}", line0);
-        println!("2: {}", line1);
-        println!("3: {}", line2);
-
         let badge_prio = line0
             .chars()
             .filter(|c| line1.contains(|c1| &c1 == c))
             .filter(|c| line2.contains(|c2| &c2 == c))
             .take(1)
             .map(|badge| {
-                println!(" badge: {}", badge);
                 let value = badge as u32;
                 if value >= LOWER_A_VALUE && value <= LOWER_Z_VALUE {
                     value - LOWER_A_VALUE + 1
@@ -67,8 +57,6 @@ fn solve_part2(input: &str) -> u32 {
             })
             .next()
             .unwrap();
-
-        println!("prio: {}\n", badge_prio);
 
         sum += badge_prio;
     }
