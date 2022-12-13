cd26f02dd9ce321492ab7cc4e571a1ee95dfc6cc
Andreas Andersson <andreas@neoboid.com>
2022-12-13 22:15:55 +0100

    11: Add main test case

--- day11/src/main.rs
+++ day11/src/main.rs
@@ -48,12 +48,37 @@ mod tests_day11 {
     use super::*;
 
     const EXAMPLE1: &str = "
-3 seals
-4 quacks";
+Monkey 0:
+  Starting items: 79, 98
+  Operation: new = old * 19
+  Test: divisible by 23
+    If true: throw to monkey 2
+    If false: throw to monkey 3
+
+Monkey 1:
+  Starting items: 54, 65, 75, 74
+  Operation: new = old + 6
+  Test: divisible by 19
+    If true: throw to monkey 2
+    If false: throw to monkey 0
+
+Monkey 2:
+  Starting items: 79, 60, 97
+  Operation: new = old * old
+  Test: divisible by 13
+    If true: throw to monkey 1
+    If false: throw to monkey 3
+
+Monkey 3:
+  Starting items: 74
+  Operation: new = old + 3
+  Test: divisible by 17
+    If true: throw to monkey 0
+    If false: throw to monkey 1";
 
     #[test]
     fn test1_1() {
-        assert_eq!(solve_part1(EXAMPLE1), 7);
+        assert_eq!(solve_part1(EXAMPLE1), 10605);
     }
 
     const EXAMPLE2: &str = "";
