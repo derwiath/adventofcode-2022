4305f77915d41b46ed1b87ff7ae44171fbfdba56
Andreas Andersson <andreas@neoboid.com>
2022-12-22 22:58:49 +0100

    14: Add and parse Vector2

--- day14/src/main.rs
+++ day14/src/main.rs
@@ -5,7 +5,53 @@ extern crate lazy_static;
 extern crate regex;
 
 use std::env;
+use std::fmt;
 use std::fs;
+use std::str::FromStr;
+
+#[derive(PartialEq, Eq, Clone, Copy)]
+struct Vector2 {
+    x: isize,
+    y: isize,
+}
+
+impl Vector2 {
+    fn new(x: isize, y: isize) -> Vector2 {
+        Vector2 { x, y }
+    }
+}
+
+impl FromStr for Vector2 {
+    type Err = &'static str;
+    fn from_str(input: &str) -> Result<Vector2, &'static str> {
+        let mut it = input.split(",");
+        let x = if let Some(x_str) = it.next() {
+            if let Ok(x) = x_str.parse::<usize>() {
+                x
+            } else {
+                return Err("Failed to parse x");
+            }
+        } else {
+            return Err("No x in str");
+        };
+        let y = if let Some(x_str) = it.next() {
+            if let Ok(y) = x_str.parse::<usize>() {
+                y
+            } else {
+                return Err("Failed to parse y");
+            }
+        } else {
+            return Err("No y in str");
+        };
+        Ok(Vector2::new(x as isize, y as isize))
+    }
+}
+
+impl fmt::Debug for Vector2 {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "({}, {})", self.x, self.y)
+    }
+}
 
 fn solve_part1(input: &str) -> usize {
     lazy_static! {
@@ -57,6 +103,11 @@ mod tests_day14 {
     #[test]
     fn test1_1() {
         assert_eq!(solve_part1(EXAMPLE1), 24);
+    }
+
+    #[test]
+    fn test1_vector2_from_str_1() {
+        assert_eq!(Vector2::from_str("498,4"), Ok(Vector2::new(498, 4)));
     }
 
     const EXAMPLE2: &str = "";
