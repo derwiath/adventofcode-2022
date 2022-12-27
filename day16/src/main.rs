d6229adff2793b558f0c7b7a3d1d7eec69dba104
Andreas Andersson <andreas@neoboid.com>
2022-12-27 17:41:14 +0100

    16: Add test input

--- day16/src/main.rs
+++ day16/src/main.rs
@@ -50,12 +50,21 @@ mod tests_day16 {
     use super::*;
 
     const EXAMPLE1: &str = "
-3 seals
-4 quacks";
+Valve AA has flow rate=0; tunnels lead to valves DD, II, BB
+Valve BB has flow rate=13; tunnels lead to valves CC, AA
+Valve CC has flow rate=2; tunnels lead to valves DD, BB
+Valve DD has flow rate=20; tunnels lead to valves CC, AA, EE
+Valve EE has flow rate=3; tunnels lead to valves FF, DD
+Valve FF has flow rate=0; tunnels lead to valves EE, GG
+Valve GG has flow rate=0; tunnels lead to valves FF, HH
+Valve HH has flow rate=22; tunnel leads to valve GG
+Valve II has flow rate=0; tunnels lead to valves AA, JJ
+Valve JJ has flow rate=21; tunnel leads to valve II
+";
 
     #[test]
     fn test1_1() {
-        assert_eq!(solve_part1(EXAMPLE1), 7);
+        assert_eq!(solve_part1(EXAMPLE1), 1651);
     }
 
     const EXAMPLE2: &str = "";
