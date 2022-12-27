0cc02c330af18fd2e286c99f8013d0aed7092d8b
Andreas Andersson <andreas@neoboid.com>
2022-12-27 18:10:30 +0100

    16: Parse valve row

--- day16/src/main.rs
+++ day16/src/main.rs
@@ -7,6 +7,38 @@ extern crate regex;
 use std::env;
 use std::fs;
 
+#[derive(PartialEq, Debug)]
+struct Valve<'a> {
+    name: &'a str,
+    flow: usize,
+    neighbours: Vec<&'a str>,
+}
+
+impl<'a> Valve<'a> {
+    fn new(name: &'a str, flow: usize, neighbours: Vec<&'a str>) -> Valve<'a> {
+        Valve {
+            name,
+            flow,
+            neighbours,
+        }
+    }
+
+    fn from_str(s: &'a str) -> Valve<'a> {
+        lazy_static! {
+            static ref RE: regex::Regex = regex::Regex::new(
+                r"Valve ([A-Z]*) has flow rate=(\d*); tunnels lead to valves ([A-Z, ]*)"
+            )
+            .unwrap();
+        }
+        let captures = RE.captures(s).unwrap();
+        assert_eq!(captures.len(), 4);
+        let name = captures.get(1).unwrap().as_str();
+        let flow: usize = captures.get(2).unwrap().as_str().parse::<usize>().unwrap();
+        let neighbours: Vec<&'a str> = captures.get(3).unwrap().as_str().split(", ").collect();
+        Valve::new(name, flow, neighbours)
+    }
+}
+
 fn solve_part1(input: &str) -> usize {
     lazy_static! {
         static ref RE: regex::Regex = regex::Regex::new(r"(\d*) ([a-z]*)").unwrap();
@@ -65,6 +97,14 @@ Valve JJ has flow rate=21; tunnel leads to valve II
     #[test]
     fn test1_1() {
         assert_eq!(solve_part1(EXAMPLE1), 1651);
+    }
+
+    #[test]
+    fn test1_valve_from_str1() {
+        assert_eq!(
+            Valve::from_str("Valve AA has flow rate=123; tunnels lead to valves DD, II, BB"),
+            Valve::new("AA", 123, vec!["DD", "II", "BB"])
+        );
     }
 
     const EXAMPLE2: &str = "";
